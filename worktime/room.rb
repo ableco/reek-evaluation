497b6ba70bc1b17d4a4eefcd81f032cbe9dd0a08
Cristhiam Teran <foxhard@gmail.com>
2020-11-27 17:54:34 -0500

    Apply Reek on worktime file

--- worktime/room.rb
+++ worktime/room.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
+class Participant
+  def self.room_identifier(participant)
+    return participant if participant.is_a?(String)
+
+    participant['id']
+  end
+end
+
 class Room < ApplicationRecord
   belongs_to :user, optional: true
   has_many :room_users
@@ -11,32 +21,31 @@ class Room < ApplicationRecord
   after_create :create_canvas
 
   def related_organization_domain
-    user&.organization_domain || "gmail.com"
+    user&.organization_domain || 'gmail.com'
+  end
+
+  def set_user_audio(user)
+    if participants_metadata.first.is_a?(Hash)
+      if participant_metadata = participants_metadata.find { |participant| participant['id'] == user.room_identifier }
+        user.audio = participant_metadata['audio'].to_s == 'true'
+      end
+    end
   end
 
   def current_participants
     User.where(room_identifier: participants_room_identifiers).select(:id, :room_identifier, :avatar_url, :name).map do |user|
-      if participants_metadata.first.is_a?(Hash)
-        if participant_metadata = participants_metadata.find { |participant| participant["id"] == user.room_identifier }
-          user.audio = participant_metadata["audio"].to_s == "true"
-        end
-      end
-
-      user.as_json(only: [:id, :room_identifier, :avatar_url, :name, :acronym_name])
+      set_user_audio(user)
+      user.as_json(only: %i[id room_identifier avatar_url name acronym_name])
     end
   end
 
   def participants_metadata
-    metadata["participants"] || []
+    metadata['participants'] || []
   end
 
   def participants_room_identifiers
-    metadata["participants"].map do |participant|
-      if participant.is_a?(String)
-        participant
-      else
-        participant["id"]
-      end
+    metadata['participants'].map do |participant|
+      Participant.room_identifier(participant)
     end
   end
 
